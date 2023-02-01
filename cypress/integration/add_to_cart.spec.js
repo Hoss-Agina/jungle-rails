c468709a3284812e8bcf79989550ad1187c0cd3b
Hoss Agina <hoss.agina@telus.com>
2023-01-31 23:29:54 -0700

    screenshots now in docs folder

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -9,9 +9,10 @@ describe('example to-do app', () => {
 
   it("clicks on the product to go the product view page", () => {
     cy.get(".products .button_to .btn").first().click({force: true})
-  })
+  });
 
   it("checks that cart now displays 1 once a product is added", () => {
     cy.get("a.nav-link").contains("My Cart").should("contain","1")
-  })
+  });
+  
 });
